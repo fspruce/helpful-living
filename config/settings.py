a366da9e66fa0b6be5bb694b4ba890d00c493e3e
fspruce <f.spruce@proton.me>
2025-09-25 15:05:29 +0100

    Set up homepage url, extend base code from base.html

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "dal",
     "dal_select2",
     # My Apps
+    "core",
 ]
 
 # Site ID for django.contrib.sites
--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """Renders the index.html page for the homepage"""
+    return render(request, 'core/index.html')
